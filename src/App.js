7f4169c215e56a825402283c3c8581090882140b
Juho Lehtonen <Juho.p.lehtonen@eng.tamk.fi>
2017-11-13 21:12:14 +0200

    Fixed api key retardation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react"
 const fetch = require("isomorphic-fetch");
-const { compose, withProps, withHandlers, withStateHandlers } = require("recompose");
-const FaAnchor = require("react-icons/lib/fa/anchor");
+const { compose, withProps, withHandlers } = require("recompose");
+//const FaAnchor = require("react-icons/lib/fa/anchor");
 const {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker,
-  InfoWindow
+//  InfoWindow
 } = require("react-google-maps");
 
 const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 
 const MapWithAMarkerClusterer = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBkBrkFqSna6gn9WzSNqDJT8K-DFot1Fzk&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `100vh` }} />,
     mapElement: <div style={{ height: `100%` }} />,
@@ -58,7 +58,7 @@ class DemoApp extends React.PureComponent {
   componentDidMount() {
     const url = "http://dropa.asuscomm.com/api/devices-and-detectors";
 
-    fetch(url, {method: 'GET',})
+    fetch(url, {method: 'GET', headers:{ 'Accept':'application/x-www-form-urlencoded', 'Content-Type':'application/x-www-form-urlencoded',}})
       .then(res => res.json())
       .then(data => {
         this.setState({ markers: data });
