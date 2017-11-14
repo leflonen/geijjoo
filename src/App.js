5e0938aeba3d123319c4c36160e04fed2aa1585e
Sami Hellsten <sami.hellsten@eng.tamk.fi>
2017-11-14 19:32:34 +0200

    Try to identify markers

--- src/App.js
+++ src/App.js
@@ -14,42 +14,43 @@ const {
 const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 
 const StyledMapWithAMarkerClusterer = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBkBrkFqSna6gn9WzSNqDJT8K-DFot1Fzk&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100vh` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withHandlers({
-    onMarkerClustererClick: () => (markerClusterer) => {
-      const clickedMarkers = markerClusterer.getMarkers()
-      console.log(`Current clicked markers length: ${clickedMarkers.length}`)
-      console.log(clickedMarkers)
-    },
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBkBrkFqSna6gn9WzSNqDJT8K-DFot1Fzk&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `100vh` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
     }),
-  withScriptjs,
-  withGoogleMap
+    withHandlers({
+        onMarkerClustererClick: () => (markerClusterer) => {
+        },
+        onMarkerClick: () => (val) => {
+          console.log("ugh " + val.id);
+        }
+    }),
+    withScriptjs,
+    withGoogleMap
 )(props =>
-  <GoogleMap
-    defaultZoom={13}
-    defaultCenter={{ lat: 61.49911, lng: 23.78712 }}
-    defaultOptions={{ styles: demoFancyMapStyles}}
-  >
-    <MarkerClusterer
-      onClick={props.onMarkerClustererClick}
-      averageCenter
-      enableRetinaIcons
-      gridSize={60}
+    <GoogleMap
+        defaultZoom={13}
+        defaultCenter={{ lat: 61.49911, lng: 23.78712 }}
+        defaultOptions={{ styles: demoFancyMapStyles}}
     >
-      {props.markers.map(marker => (
-        <Marker
-          key={marker.id}
-          position={{ lat: parseFloat(marker.field_longitude), lng: parseFloat(marker.field_latitude) }}
-        >
-      </Marker>
-      ))}
-    </MarkerClusterer>
-  </GoogleMap>
+      <MarkerClusterer
+          onClick={props.onMarkerClustererClick}
+          averageCenter
+          enableRetinaIcons
+          gridSize={60}
+      >
+          {props.markers.map(marker => (
+              <Marker
+                  key={marker.id}
+                  onMouseOver={props.onMarkerClick}
+                  position={{ lat: parseFloat(marker.field_longitude), lng: parseFloat(marker.field_latitude) }}
+              >
+              </Marker>
+          ))}
+      </MarkerClusterer>
+    </GoogleMap>
 );
 
 export default class App extends React.PureComponent {
